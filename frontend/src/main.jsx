1d6a594db4c1cb355364eba304be86b8eaa28215
Jamil-4khtar <jamil4khtar@gmail.com>
2025-08-03 00:29:31 +0530

    fix: correct import path case for DashboardProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import "./styles/index.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./contexts/AuthProvider.jsx";
 import { BrowserRouter } from "react-router-dom";
-import DashboardProvider from "./contexts/dashboardProvider.jsx";
+import DashboardProvider from "./contexts/DashboardProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
